e8fe3cbe31966b7a4c1fc4f7d06002266cfc3ebb
oldmud0 <oldmud0@users.noreply.github.com>
2017-06-13 17:09:29 -0500

    Create persistence file's skeleton with an interface

---
+++ src/codeu/chat/server/PersistenceFileSkeleton.java
@@ -0,0 +1,29 @@
+package codeu.chat.server;
+
+import java.util.Collection;
+import java.util.Map;
+
+import codeu.chat.common.ConversationHeader;
+import codeu.chat.common.ConversationPayload;
+import codeu.chat.common.Message;
+import codeu.chat.common.Secret;
+import codeu.chat.common.User;
+import codeu.chat.util.Uuid;
+
+public interface PersistenceFileSkeleton {
+  public interface ServerInfo {
+    Uuid id();
+    Secret secret();
+    Uuid lastSeen();
+  }
+  
+  ServerInfo serverInfo();
+  
+  Map<Uuid, User> users();
+  
+  Map<Uuid, ConversationHeader> conversationHeaders();
+  Map<Uuid, ConversationPayload> conversationPayloads();
+  
+  Map<Uuid, Message> messages();
+  
+}
