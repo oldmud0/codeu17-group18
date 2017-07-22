f0f9000bdded3f6eb7379a8028d19d000c1e3f6a
oldmud0 <oldmud0@users.noreply.github.com>
2017-07-22 16:51:11 -0500

    Format ConversationSecurityDescriptor

--- src/codeu/chat/security/ConversationSecurityDescriptor.java
+++ src/codeu/chat/security/ConversationSecurityDescriptor.java
@@ -6,41 +6,45 @@ import java.util.Map;
 import codeu.chat.util.Uuid;
 
 public class ConversationSecurityDescriptor {
-	private int implicitSecurity;
-	private Map<Uuid, Integer> explicitSecurity = new HashMap<>();
+  private int implicitSecurity;
+  private Map<Uuid, Integer> explicitSecurity = new HashMap<>();
+  
+  public ConversationSecurityDescriptor(Uuid owner) {
+    explicitSecurity.put(owner, ConversationSecurityPresets.CREATOR);
+  }
 
-	public void setPermissions(Uuid invoker, Uuid target, int flags) throws SecurityViolationException {
-		if (hasFlags(invoker, ConversationSecurityFlags.MODIFY_SECURITY) == false) {
-			throw new SecurityViolationException("You do not have access to set permissions.");
-		} else if (getEffectivePermissions(invoker) <= getEffectivePermissions(target)) {
-			throw new SecurityViolationException("The user you have to tried to change has equal or higher permissions.");
-		} else if (invoker.equals(target)) {
-			throw new SecurityViolationException("You cannot change your own permissions.");
-		} else {
-			explicitSecurity.put(target, flags);
-		}
-	}
+  public void setPermissions(Uuid invoker, Uuid target, int flags) throws SecurityViolationException {
+    if (hasFlags(invoker, ConversationSecurityFlags.MODIFY_SECURITY) == false) {
+      throw new SecurityViolationException("You do not have access to set permissions.");
+    } else if (getEffectivePermissions(invoker) <= getEffectivePermissions(target)) {
+      throw new SecurityViolationException("The user you have to tried to change has equal or higher permissions.");
+    } else if (invoker.equals(target)) {
+      throw new SecurityViolationException("You cannot change your own permissions.");
+    } else {
+      explicitSecurity.put(target, flags);
+    }
+  }
 
-	public void resetPermissions(Uuid invoker, Uuid target) throws SecurityViolationException {
-		if (hasFlags(invoker, ConversationSecurityFlags.MODIFY_SECURITY) == false) {
-			throw new SecurityViolationException("You do not have access to set permissions.");
-		} else if (getEffectivePermissions(invoker) <= getEffectivePermissions(target)) {
-			throw new SecurityViolationException("The user you have to tried to change has equal or higher permissions.");
-		} else if (invoker.equals(target)) {
-			throw new SecurityViolationException("You cannot change your own permissions.");
-		} else {
-			explicitSecurity.remove(target);
-		}
-	}
+  public void resetPermissions(Uuid invoker, Uuid target) throws SecurityViolationException {
+    if (hasFlags(invoker, ConversationSecurityFlags.MODIFY_SECURITY) == false) {
+      throw new SecurityViolationException("You do not have access to set permissions.");
+    } else if (getEffectivePermissions(invoker) <= getEffectivePermissions(target)) {
+      throw new SecurityViolationException("The user you have to tried to change has equal or higher permissions.");
+    } else if (invoker.equals(target)) {
+      throw new SecurityViolationException("You cannot change your own permissions.");
+    } else {
+      explicitSecurity.remove(target);
+    }
+  }
 
-	public boolean hasFlags(Uuid invoker, int flags) {
-		return (getEffectivePermissions(invoker) & flags) == flags;
-	}
+  public boolean hasFlags(Uuid invoker, int flags) {
+    return (getEffectivePermissions(invoker) & flags) == flags;
+  }
 
-	private int getEffectivePermissions(Uuid id) {
-		if (explicitSecurity.get(id) == null) {
-			return implicitSecurity;
-		}
-		return explicitSecurity.get(id);
-	}
+  private int getEffectivePermissions(Uuid id) {
+    if (explicitSecurity.get(id) == null) {
+      return implicitSecurity;
+    }
+    return explicitSecurity.get(id);
+  }
 }
